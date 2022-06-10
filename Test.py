42a5fc60815e961ba7ecd243fdc5e8ca107a8ae8
sandhyakumari poduri <poduri.sandhya120@gmail.com>
2022-06-10 23:43:00 +0530

    Adding Python

---
+++ Test.py
@@ -0,0 +1,15 @@
+import os
+l1= [1,2,3,4]
+l1.reverse()
+print('The Reversed List is', l1)
+
+l2=[5,1,8,2,9]
+l2.sort()
+print('The Sorting of list', l2)
+
+l3=[]
+l3=l2.copy()
+print('List',l3)
+
+indexvalue= l2[2:6]
+print('The index value is', indexvalue)
